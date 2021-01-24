4840acc4eab3fc1ed9530a93e90e3ef8db774fe1
Rodrigo Candido <rodrigoipu@hotmail.com>
2021-01-24 10:52:45 +0100

    CODE REFACTOR Clean Comment new data managers  and network services related code

--- DiscourseApp/DataManagers/RemoteDataManagerImpl.swift
+++ DiscourseApp/DataManagers/RemoteDataManagerImpl.swift
@@ -9,11 +9,7 @@ import UIKit
 
 
 class RemoteDataManagerImpl: RemoteDataManager{
-
-
     
-    
-
     // MARK: - Class Properties
     let networkService: NetworkService
     
@@ -21,7 +17,7 @@ class RemoteDataManagerImpl: RemoteDataManager{
     init(networkService: NetworkService) {
         self.networkService = networkService
     }
-        
+    
     // MARK: - TOPICS RELATED IMPLEMENTATION
     func fetchAllTopics(completion: @escaping (Result<LatestTopicsResponse?, Error>) -> ()) {
         let request = LatestTopicsResquest()
@@ -33,17 +29,15 @@ class RemoteDataManagerImpl: RemoteDataManager{
         self.networkService.fetch(this: request, for: completion)
     }
     
-    
     func deleteTopic(id: Int, completion: @escaping (Result<DeleteTopicResponse?, Error>) -> ()) {
         let request = DeleteTopicResquest(id: id)
         self.networkService.fetch(this: request, for: completion)
     }
-
+    
     func addNewTopic(title: String, raw: String, completion: @escaping (Result<AddNewTopicResponse?, Error>) -> ()) {
         let request = AddNewTopicRequest(title: title, raw: raw)
         self.networkService.fetch(this: request, for: completion)
     }
-    
     
     // MARK: - CATEGORY RELATED IMPLEMENTATION
     func fetchCategories(completion: @escaping (Result<CategoryResponse?, Error>) -> ()) {
@@ -64,21 +58,16 @@ class RemoteDataManagerImpl: RemoteDataManager{
         self.networkService.fetchImage(imageURL: url, completion: completion)
     }
     
-    
     func fetchSingleUser(userName: String, completion: @escaping (Result<SingleUserResponse?, Error>) -> ()) {
-    
+        
         let request = SingleUserRequest(userName: userName)
         self.networkService.fetch(this: request, for: completion)
     }
-    
     
     func updateUser(userName: String, newName: String, completion: @escaping (Result<SingleUserResponse?, Error>) -> ()) {
         let request = UpdataSingleUserRequest(userName: userName, newName: newName)
         self.networkService.fetch(this: request, for: completion
         )
     }
-    
-
-    
     
 }
